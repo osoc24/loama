02f3582e578a30a2fb43900c02ab5a1dfc20106f
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-07-17 14:17:13 +0200

    Added fallback loading animation for when ResourceExplorer is loading

---
+++ loama/src/components/explorer/FallbackExplorer.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <span></span>
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    background-color: var(--off-white);
+    height: calc(100vh - calc(var(--base-unit)* 14));
+}
+
+span {
+    display: block;
+    width: 60px;
+    aspect-ratio: 1;
+    --gradient: conic-gradient(from -90deg at 10px 10px, var(--solid-purple) 90deg, #0000 0);
+    background: var(--gradient), var(--gradient), var(--gradient);
+    background-size: 50% 50%;
+    animation: l16 1s infinite;
+}
+
+@keyframes l16 {
+    0% {
+        background-position: 0 0, 10px 10px, 20px 20px
+    }
+
+    50% {
+        background-position: 0 20px, 10px 10px, 20px 0
+    }
+
+    100% {
+        background-position: 20px 20px, 10px 10px, 0 0
+    }
+}
+</style>
---
+++ loama/src/components/explorer/ResourceExplorer.vue
@@ -0,0 +1,13 @@
+<template>
+   <p>Test</p>
+</template>
+
+<script setup lang="ts">
+import { store } from "@/store";
+import type { Session } from "@inrupt/solid-client-authn-browser";
+import { listPods, getOrCreateIndex } from "loama-controller";
+import { ref } from "vue";
+
+const pods = await listPods(store.session as Session);
+const indexFile = ref(await getOrCreateIndex(store.session as Session, pods[0])); // .then((index) => addPermissions(store.session, index, ["example.com"], true, [Permission.Read]))
+</script>
