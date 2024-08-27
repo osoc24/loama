9db57d87599d35bbacccb7ae52cbff69a052f9b5
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-08-27 16:28:18 +0200

    chore(controller): move types file into designated folder

--- controller/src/types/index.ts
+++ controller/src/types/index.ts
@@ -2,16 +2,13 @@ import { url } from "loama-common";
 
 /**
  * The json schema for the index configuration file inside a Solid data pod.
- * Generated from `schema.json`.
  */
-
 export interface Index {
     id: url;
     items: IndexItem[];
 }
 
-// NOTE: Take another look at the K generic, Will be a pity if we just use "string" here
-export interface IndexItem<K extends string = string, T extends BaseSubject<K> = BaseSubject<K>> {
+export interface IndexItem<T extends BaseSubject<string> = BaseSubject<string>> {
     id: string; // UUID
     requestId: string; // UUID
     isEnabled: boolean;
@@ -39,7 +36,6 @@ export interface SubjectPermissions<T = BaseSubject<string>> {
     permissions: Permission[]
 }
 
-// NOTE: Same note about K as above
 export interface ResourcePermissions<T = BaseSubject<string>> {
     resourceUrl: url;
     permissionsPerSubject: SubjectPermissions<T>[]
